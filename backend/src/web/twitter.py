d253eac125cdf7e4d0098f341532d93676a24947
Ashish D'Souza <sudouser512@gmail.com>
2021-02-13 02:13:40 -0500

    Updated Twitter web scraper

--- backend/src/web/twitter.py
+++ backend/src/web/twitter.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 
 
-def web_scrape(username, threshold, headless=False):
+def get_tweets(username, threshold, headless=False):
     options = Options()
     options.headless = headless
     profile = webdriver.FirefoxProfile('/home/ashish/.mozilla/firefox/3kv6pmol.default-release')
@@ -19,18 +19,19 @@ def web_scrape(username, threshold, headless=False):
     tweets = set(x.text.replace('\n', ' ') for x in browser.find_elements_by_css_selector('div.css-901oao.r-18jsvk2.r-1qd0xha.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-bnwqim.r-qvutc0'))
 
     body = browser.find_element_by_tag_name('body')
-    while len(tweets) < threshold:
+    page_end = 0
+    while len(tweets) < threshold and page_end < 2:
         body.send_keys(Keys.END)
         sleep(3)
+        previous_len = len(tweets)
         tweets.update(set(x.text.replace('\n', ' ') for x in browser.find_elements_by_css_selector('div.css-901oao.r-18jsvk2.r-1qd0xha.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-bnwqim.r-qvutc0')))
+        if previous_len == len(tweets):
+            page_end += 1
+        else:
+            page_end = 0
 
     browser.close()
     if os.path.exists('geckodriver.log'):
         os.remove('geckodriver.log')
 
-    print(len(tweets))
-    for tweet in tweets:
-        print(tweet)
-
-
-web_scrape('mtgreenee', 100, True)
+    return list(tweets)[:threshold]
