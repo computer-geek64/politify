2e296b4c098e6111b5bc706ddc231c28a9684e32
Ashish D'Souza <sudouser512@gmail.com>
2021-02-14 10:50:13 -0500

    Finished Google Cloud sentiment political analysis

--- backend/src/ml/google_cloud/nlp.py
+++ backend/src/ml/google_cloud/nlp.py
@@ -12,73 +12,97 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = GOOGLE_APPLICATION_CREDENTIALS_LO
 client = language_v1.LanguageServiceClient()
 
 
-def get_sentiment_prediction(texts):
+def get_sentiment_predictions(texts):
+    predictions = []
     for text in texts:
         document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
-        sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
+        sentiments = client.analyze_sentiment(request={'document': document})
+        score = 0
+        for sentence in sentiments.sentences:
+            for keyword in keywords:
+                if keyword['phrase'].replace(' ', '') in sentence.text.content.lower().replace(' ', ''):
+                    if sentence.sentiment.score <= -0.25:
+                        if keyword['negative'] == 'left':
+                            score -= keyword['priority']
+                        else:
+                            score += keyword['priority']
+                    elif sentence.sentiment.score >= 0.25:
+                        if keyword['positive'] == 'left':
+                            score -= keyword['priority']
+                        else:
+                            score += keyword['priority']
+                    break
+        if score < 0:
+            predictions.append('left')
+        elif score > 0:
+            predictions.append('right')
+        else:
+            predictions.append('non-political')
+    return predictions
 
 
 keywords = [
-    {'phrase': 'biden', 'positive': 'left', 'negative': 'right', 'priority': 'high'},
-    {'phrase': 'turmp', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'maga', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'prolife', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'pro-life', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'pro life', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'antiabortion', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'anti-abortion', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'anti abortion', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'abortion', 'positive': 'left', 'negative': 'right', 'priority': 'high'},
-    {'phrase': 'gun control', 'positive': 'left', 'negative': 'right', 'priority': 'high'},
-    {'phrase': 'guns', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': '2nd amendment', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'second amendment', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    #{'phrase': 'economy', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'budget deficit', 'positive': 'left', 'negative': 'right', 'priority': 'high'},
-    {'phrase': 'free healthcare', 'positive': 'left', 'negative': 'right', 'priority': 'high'},
-    {'phrase': 'socialism', 'positive': 'left', 'negative': 'right', 'priority': 'high'},
-    {'phrase': 'socialist', 'positive': 'left', 'negative': 'right', 'priority': 'high'},
-    {'phrase': 'supreme court', 'positive': 'right', 'negative': 'left', 'priority': 'medium'},
-    {'phrase': 'foreign policy', 'positive': 'left', 'negative': 'right', 'priority': 'high'},
-    {'phrase': 'antiimmigration', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'anti-immigration', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'anti immigration', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'immigration', 'positive': 'left', 'negative': 'right', 'priority': 'high'},
-    {'phrase': 'racism', 'positive': 'right', 'negative': 'left', 'priority': 'medium'},
-    {'phrase': 'climate', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'global warming', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'coal', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'petroleum', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'mining', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'natural gas', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'domestic terrorism', 'positive': 'right', 'negative': 'left', 'priority': 'high'},
-    {'phrase': 'steny hoyer', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'jim clyburn', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'katherine clark', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'hakeem jeffries', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'pete aguilar', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'sean patrick maloney', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'matt cartwright', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'debbie dingell', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'ted lieu', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'joe neguse', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'colin allred', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'mondaire jones', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'cheri bustos', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'barbara lee', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'eric swalwell', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'butterfield', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'jan schakowsky', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'steve scalise', 'positive': 'right', 'negative': 'left', 'priority': 'low'},
-    {'phrase': 'liz cheney', 'positive': 'right', 'negative': 'left', 'priority': 'low'},
-    {'phrase': 'mike johnson', 'positive': 'right', 'negative': 'left', 'priority': 'low'},
-    {'phrase': 'rich hudson', 'positive': 'right', 'negative': 'left', 'priority': 'low'},
-    {'phrase': 'gary palmer', 'positive': 'right', 'negative': 'left', 'priority': 'low'},
-    {'phrase': 'tom emmer', 'positive': 'right', 'negative': 'left', 'priority': 'low'},
-    {'phrase': 'drew ferguson', 'positive': 'right', 'negative': 'left', 'priority': 'low'},
-    {'phrase': 'impeach', 'positive': 'left', 'negative': 'right', 'priority': 'high'},
-    {'phrase': 'senate', 'positive': 'left', 'negative': 'right', 'priority': 'low'},
-    {'phrase': 'stimulus', 'positive': 'left', 'negative': 'right', 'priority': 'high'},
-    {'phrase': 'health insurance', 'positive': 'left', 'negative': 'right', 'priority': 'high'},
-    {'phrase': 'social security', 'positive': 'left', 'negative': 'right', 'priority': 'high'}
+    {'phrase': 'biden', 'positive': 'left', 'negative': 'right', 'priority': 3},
+    {'phrase': 'trump', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'maga', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'make america great again', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'prolife', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'pro-life', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'pro life', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'antiabortion', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'anti-abortion', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'anti abortion', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'abortion', 'positive': 'left', 'negative': 'right', 'priority': 3},
+    {'phrase': 'gun control', 'positive': 'left', 'negative': 'right', 'priority': 3},
+    {'phrase': 'guns', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': '2nd amendment', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'second amendment', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    #{'phrase': 'economy', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'budget deficit', 'positive': 'left', 'negative': 'right', 'priority': 3},
+    {'phrase': 'free healthcare', 'positive': 'left', 'negative': 'right', 'priority': 3},
+    {'phrase': 'socialism', 'positive': 'left', 'negative': 'right', 'priority': 3},
+    {'phrase': 'socialist', 'positive': 'left', 'negative': 'right', 'priority': 3},
+    {'phrase': 'supreme court', 'positive': 'right', 'negative': 'left', 'priority': 2},
+    {'phrase': 'foreign policy', 'positive': 'left', 'negative': 'right', 'priority': 3},
+    {'phrase': 'antiimmigration', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'anti-immigration', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'anti immigration', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'immigration', 'positive': 'left', 'negative': 'right', 'priority': 3},
+    {'phrase': 'racism', 'positive': 'right', 'negative': 'left', 'priority': 2},
+    {'phrase': 'climate', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'global warming', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'coal', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'petroleum', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'mining', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'natural gas', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'domestic terrorism', 'positive': 'right', 'negative': 'left', 'priority': 3},
+    {'phrase': 'steny hoyer', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'jim clyburn', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'katherine clark', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'hakeem jeffries', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'pete aguilar', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'sean patrick maloney', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'matt cartwright', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'debbie dingell', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'ted lieu', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'joe neguse', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'colin allred', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'mondaire jones', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'cheri bustos', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'barbara lee', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'eric swalwell', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'butterfield', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'jan schakowsky', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'steve scalise', 'positive': 'right', 'negative': 'left', 'priority': 2},
+    {'phrase': 'liz cheney', 'positive': 'right', 'negative': 'left', 'priority': 2},
+    {'phrase': 'mike johnson', 'positive': 'right', 'negative': 'left', 'priority': 2},
+    {'phrase': 'rich hudson', 'positive': 'right', 'negative': 'left', 'priority': 2},
+    {'phrase': 'gary palmer', 'positive': 'right', 'negative': 'left', 'priority': 2},
+    {'phrase': 'tom emmer', 'positive': 'right', 'negative': 'left', 'priority': 2},
+    {'phrase': 'drew ferguson', 'positive': 'right', 'negative': 'left', 'priority': 2},
+    {'phrase': 'impeach', 'positive': 'left', 'negative': 'right', 'priority': 3},
+    {'phrase': 'senate', 'positive': 'left', 'negative': 'right', 'priority': 2},
+    {'phrase': 'stimulus', 'positive': 'left', 'negative': 'right', 'priority': 3},
+    {'phrase': 'health insurance', 'positive': 'left', 'negative': 'right', 'priority': 3},
+    {'phrase': 'social security', 'positive': 'left', 'negative': 'right', 'priority': 3}
 ]
